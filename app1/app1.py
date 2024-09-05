625ceaa2d18f11d99d4d02bcd8d3bc090ea24663
Éder Pires <ederpbj@gmail.com>
2024-09-05 11:16:58 -0300

    iniciando layout 1

--- app1/app1.py
+++ app1/app1.py
@@ -4,3 +4,21 @@ import plotly.express as px
 import pandas as pd
 
 app = dash.Dash(__name__)
+
+df = pd.DataFrame({
+    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+    "Amount": [4,1,2,2,4,5],
+    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"],
+})
+
+fig = px.bar(df, x="Fruit", y="Amount", color="City")
+
+
+#============= Layout
+
+app.layout = html.Div(
+    children=[
+        html.H1("Hello Dash")
+    ]
+)
+
